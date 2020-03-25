b785363d753a8cc55ccab8870f382a723b4bcdac
dionybusker <99057959@mydavinci.nl>
2020-03-25 13:09:15 +0100

    includes dbcon.php, header.php, footer.php

---
+++ character.php
@@ -0,0 +1,24 @@
+<?php
+    include("includes/dbcon.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>B3W25O1-dynamische_applicatie</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <?php include("includes/header.php"); ?>
+
+        <main>
+        
+            <!-- code -->
+
+        </main>
+    
+        <?php include("includes/footer.php"); ?>
+        
+    </body>
+</html>
