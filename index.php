7d7349de40da16dcc9f7bb03afa0f50ca9afd23b
dionybusker <99057959@mydavinci.nl>
2020-03-25 13:39:47 +0100

    icons fontawesome toegevoegd, a href op img
    als je op de img van het karakter drukt, ga je naar de volgende pagina met dat karakter
    id is te zien in de url

--- index.php
+++ index.php
@@ -12,21 +12,26 @@
         <meta charset="UTF-8">
         <title>B3W25O1-dynamische_applicatie</title>
         <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/8eccf2802e.js" crossorigin="anonymous"></script> <!-- font awesome -->
+
     </head>
     <body>
         <?php include("includes/header.php"); ?>
 
         <main id="container">
         
+            <h1>Alle karakters op een rij</h1>
             <?php foreach ($result as $row) { ?>
                 <div class="card border solid" style="background-color:<?php echo $row['color']; ?>">
-                    <img class="avatar border solid" src="img/<?php echo $row['avatar'] ?>" alt="avatar">
+                    <a href="character.php?id=<?php echo $row['id']; ?>">
+                        <img class="avatar border solid" src="img/<?php echo $row['avatar'] ?>" alt="avatar">
+                    </a>    
                     <div class="info">
                         <h2><?php echo $row['name']; ?></h2>
                         <p>
-                            Health: <?php echo $row['health']; ?> <br>
-                            Attack: <?php echo $row['attack']; ?> <br>
-                            Defense: <?php echo $row['defense']; ?>
+                            <i class="fas fa-heart"></i> <?php echo $row['health']; ?> <br>
+                            <i class="fas fa-fist-raised"></i> <?php echo $row['attack']; ?> <br>
+                            <i class="fas fa-shield-alt"></i> <?php echo $row['defense']; ?>
                         </p>
                     </div>
 
