95ec381876f90609a43268c913f7f4a6a3611718
dionybusker <99057959@mydavinci.nl>
2020-03-27 12:24:17 +0100

    weapon/armor, extra tekst + pagina terug, borders
    - weapon en armor toegevoegd bij de characters
    - er is extra tekst bij gezet, de naam + de pagina terug staan in een div bovenaan
    - border zijn ronder gemaakt aan de onderkant

--- character.php
+++ character.php
@@ -11,29 +11,33 @@
 ?>
 
     <body>
-
         <main id="container">
-        
-
-
-
-                <div class="card border solid">
-                    <div class="character border solid" style="background-color: <?php echo $result['color']; ?>;">
+                <div class="card">
+                    <div class="character" style="background-color: <?php echo $result['color']; ?>;">
                         <img class="avatar border solid" src="img/<?php echo $result['avatar'] ?>" alt="<?php echo $result['name']; ?> avatar">
+                        <div class="chosen">
+                            <h2>U hebt gekozen voor <?php echo $result['name'] ?>!</h2>
+                            <p>Bent u niet tevreden met uw keuze? Klik <a class="bold" href="index.php">hier</a> om terug te gaan.</p>
+                        </div>
+
                         <p class="info">
                             <i class="fas fa-heart"></i> <?php echo $result['health']; ?> <br>
                             <i class="fas fa-fist-raised"></i> <?php echo $result['attack']; ?> <br>
-                            <i class="fas fa-shield-alt"></i> <?php echo $result['defense']; ?>
+                            <i class="fas fa-shield-alt"></i> <?php echo $result['defense']; ?> <br>
+
+                            <?php if ($result['weapon'] != NULL) { ?>
+                                <span class="bold">Weapon:</span> <?php echo $result['weapon']; ?> <br>
+                            <?php } ?>
+                            <?php if ($result['armor'] != NULL) { ?>
+                                <span class="bold">Armor:</span> <?php echo $result['armor']; ?>
+                            <?php } ?>
                         </p>
                     </div>
-                    <div class="bio border solid">
+                    <div class="bio">
                         <p><?php echo $result['bio']; ?></p>
                     </div>
                 </div>
-
+                <?php include("includes/footer.php"); ?>
         </main>
-    
-        <?php include("includes/footer.php"); ?>
-        
     </body>
 </html>
